5dc9523cc9e4ee74a4c786687bb6a6a79744c056
Chenyu Xiao <s3829221@student.rmit.edu.au>
2021-03-24 22:50:57 +1100

    Initial commit

---
+++ AbstractGraph.java
@@ -0,0 +1,31 @@
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+/**
+ * Abstract class to allow you to implement common functionality or definitions.
+ * All three graph implement classes extend this.
+ *
+ * Note, you should make sure to test after changes.  Note it is optional to
+ * use this file.
+ *
+ * @author Jeffrey Chan, 2021.
+ */
+public abstract class AbstractGraph implements ContactsGraph
+{
+
+    private HashMap<String, Integer> indices;
+    private HashMap<String, SIRState> sirStates;
+
+    public AbstractGraph() {
+        this.indices = new HashMap<String, Integer>();
+        this.sirStates = new HashMap<String, SIRState>();
+    }
+
+    public HashMap<String, Integer> getIndices() {
+        return indices;
+    }
+
+    public HashMap<String, SIRState> getSirStates() {
+        return sirStates;
+    }
+} // end of abstract class AbstractGraph
