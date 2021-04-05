01b03e4ec907fb98e8be0f64527c83a3e8587744
AARON <aaronmathews3009@gmail.com>
2021-04-05 20:23:20 +0530

    Commiting updated SIRModel code. -Aaron

--- SIRModel.java
+++ SIRModel.java
@@ -29,32 +29,73 @@ public class SIRModel {
 			PrintWriter sirModelOutWriter) {
 		// IMPLEMENT ME!
 		int t = 1;
+		int monitor = 0;
 		boolean cont = true;
-		String[] infected = null;
+		DynamicArray<String> infected = new DynamicArray<String>();
 		DynamicArray<String> recovered = new DynamicArray<String>();
 
 		while (cont) {
-			String[] newInfected = null;
+			DynamicArray<String> newInfected = new DynamicArray<String>();
 			DynamicArray<String> newRecovered = new DynamicArray<String>();
-			newInfected = updatedInfected(infectionProb, seedVertices);
+
+			newInfected = updatedInfected(graph, infectionProb, seedVertices);
 			// infected = infected+ newInfected; copy the elements of newInfected to
 			// infected array
+			for (int i = 0; i < newInfected.getSize(); i++) {
+				infected.add(newInfected.get(i));
+			}
+			newRecovered = updateRecovered(graph, infected, recoverProb);
+			for (int i = 0; i < newRecovered.getSize(); i++) {
+				recovered.add(newRecovered.get(i));
+			}
+			if (newRecovered.getSize() == 0 && newInfected.getSize() == 0) {
+				monitor++;
+			} else
+				monitor = 0;
 
+			if (monitor == 10)
+				cont = false;
+			else {
+				t++;
+				seedVertices = new String[infected.getSize()];
+				for (int i = 0; i < infected.getSize(); i++)
+					seedVertices[i] = infected.get(i);
+			}
+			sirModelOutWriter.print(t + ": [");
+			for (int i = 0; i < newInfected.getSize(); i++) {
+				sirModelOutWriter.print(newInfected.get(i) + " ");
+			}
+			sirModelOutWriter.print("] : [");
+			for (int i = 0; i < newRecovered.getSize(); i++) {
+				sirModelOutWriter.print(newRecovered.get(i) + " ");
+			}
+			sirModelOutWriter.print("]");
 		}
 
 	} // end of runSimulation()
 
-
-	private String[] updatedInfected(float infectionProb, String[] seedVertices) {
+	private DynamicArray<String> updatedInfected(ContactsGraph graph, float infectionProb, String[] seedVertices) {
 		// TODO Auto-generated method stub
-		String[] newseedVertices = null;
-
-		return newseedVertices;
+		AbstractGraph abstractGraph = (AbstractGraph) graph;
+		DynamicArray<String> newInfected = new DynamicArray<String>();
+		String[] temp = null;
+		for (int i = 0; i < seedVertices.length; i++) {
+			temp = graph.kHopNeighbours(1, seedVertices[i]);
+			for (int j = 0; j < temp.length; j++) {
+				if (abstractGraph.getSirStates().get(temp[i]) == SIRState.S) {
+					float random = (float) Math.random();
+					if (random <= infectionProb) {
+						graph.toggleVertexState(temp[i]);
+						newInfected.add(temp[i]);
+					}
+				}
+			}
+		}
+		return newInfected;
 	}
 
-
-	private DynamicArray<String> updateRecovered
-			(ContactsGraph graph, DynamicArray<String> infected, float recoverProb) {
+	private DynamicArray<String> updateRecovered(ContactsGraph graph, DynamicArray<String> infected,
+			float recoverProb) {
 		DynamicArray<String> newRecovered = new DynamicArray<String>();
 		for (int i = 0; i < infected.getSize(); i++) {
 			float random = (float) Math.random();
